238b11c08bb9c599d082447c181bf09cb25498d3
phoenixbart <99188309+phoenixbart@users.noreply.github.com>
2024-12-15 12:25:32 +0100

    save database in data folder

--- database/db.js
+++ database/db.js
@@ -1,6 +1,18 @@
 const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const fs = require('fs');
 
-const db = new sqlite3.Database('./database.db', (err) => {
+const dataDir = path.join(__dirname, '..', 'data');
+if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir);
+}
+
+console.log('dataDir', dataDir);
+
+
+const dbPath = path.join(__dirname, '..', 'data', 'database.db');
+
+const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
         console.error('Failed to connect to the database:', err.message);
     } else {
